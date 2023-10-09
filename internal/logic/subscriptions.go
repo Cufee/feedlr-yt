be3a42238aa3112890108e75c776614357084b8e
Cufee <vkouzin@outlook.com>
2023-10-09 17:34:04 -0500

    fixed CaughtUp flag being always set to false

--- internal/logic/subscriptions.go
+++ internal/logic/subscriptions.go
@@ -54,22 +54,20 @@ func GetUserSubscriptionsProps(userId string) (*types.UserSubscriptionsFeedProps
 	for _, c := range channels {
 		channelIds = append(channelIds, c.ID)
 	}
-	videos, err := GetChannelVideos(channelIds...)
+	allVideos, err := GetChannelVideos(channelIds...)
 	if err != nil {
 		return nil, err
 	}
 
 	// Map videos to channel IDs
 	channelVideos := make(map[string][]types.VideoProps)
-	for _, v := range videos {
-		videos := channelVideos[v.ChannelID]
+	for _, v := range allVideos {
 		// Limit videos to 3
-		if len(videos) >= 3 {
+		if len(channelVideos[v.ChannelID]) >= 3 {
 			continue
 		}
 		v.Progress = progress[v.ID]
-		videos = append(videos, v)
-		channelVideos[v.ChannelID] = videos
+		channelVideos[v.ChannelID] = append(channelVideos[v.ChannelID], v)
 	}
 
 	// Sort channels for subscription feed props
@@ -80,8 +78,8 @@ func GetUserSubscriptionsProps(userId string) (*types.UserSubscriptionsFeedProps
 
 		props.CaughtUp = true
 		for _, v := range props.Videos {
-			props.CaughtUp = false
 			if v.Progress < 1 {
+				props.CaughtUp = false
 				break
 			}
 		}
