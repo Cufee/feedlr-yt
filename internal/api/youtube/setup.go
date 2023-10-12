d342796409685b9bf0453325cb16b2937d686ffb
Cufee <vkouzin@outlook.com>
2023-10-11 19:51:33 -0500

    removed unused code

--- internal/api/youtube/setup.go
+++ internal/api/youtube/setup.go
@@ -5,36 +5,8 @@ import (
 
 	"github.com/byvko-dev/youtube-app/internal/api/youtube/client"
 	"github.com/byvko-dev/youtube-app/internal/api/youtube/internal/google"
-	"github.com/byvko-dev/youtube-app/internal/api/youtube/internal/invidious"
-	"github.com/byvko-dev/youtube-app/internal/api/youtube/internal/unified"
 	_ "github.com/joho/godotenv/autoload"
 )
 
-var C client.YouTube
+var C client.YouTube = google.NewClient(os.Getenv("YOUTUBE_API_KEY"))
 var Client = C
-
-func InitGoogleClient() {
-	C = google.NewClient(os.Getenv("YOUTUBE_API_KEY"))
-}
-
-func InitInvidiousClient() {
-	C = invidious.NewClient(os.Getenv("INVIDIOUS_HOST"))
-}
-
-func InitUnifiedClient() {
-	C = unified.NewClient()
-}
-
-func init() {
-	flavor := os.Getenv("YOUTUBE_API_FLAVOR")
-	switch flavor {
-	case "google":
-		InitGoogleClient()
-	case "unified":
-		InitInvidiousClient()
-	case "invidious":
-		InitInvidiousClient()
-	default:
-		InitGoogleClient()
-	}
-}
