4d2f5f4d64869076dc0e57bd4eb526ca5db0dd17
Cufee <vkouzin@outlook.com>
2023-10-11 19:48:49 -0500

    fixed youtube video search

--- go.mod
+++ go.mod
@@ -67,6 +67,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.49.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	golang.org/x/sys v0.13.0 // indirect
 	google.golang.org/api v0.143.0
 )
---
+++ internal/api/youtube/internal/google/playlists.go
@@ -0,0 +1,86 @@
+package google
+
+import (
+	"errors"
+	"sort"
+	"sync"
+
+	yt "github.com/byvko-dev/youtube-app/internal/api/youtube/client"
+	"google.golang.org/api/youtube/v3"
+)
+
+func (c *client) GetChannelUploadPlaylistID(channelId string) (string, error) {
+	playlists, err := c.service.Channels.List([]string{"id", "contentDetails"}).Id(channelId).Fields("items(contentDetails/relatedPlaylists/uploads)").Do()
+	if err != nil {
+		return "", err
+	}
+
+	if len(playlists.Items) <= 0 {
+		return "", errors.New("channel not found")
+	}
+
+	return playlists.Items[0].ContentDetails.RelatedPlaylists.Uploads, nil
+}
+
+func (c *client) GetPlaylistVideos(playlistId string, limit int) ([]yt.Video, error) {
+	if limit < 1 {
+		limit = 3
+	}
+
+	res, err := c.service.PlaylistItems.List([]string{"id", "snippet"}).PlaylistId(playlistId).MaxResults(int64(limit * 3)).Do()
+	if err != nil {
+		return nil, err
+	}
+
+	var wg sync.WaitGroup
+	var errChan = make(chan error, len(res.Items))
+	var validVideos = make(chan *youtube.PlaylistItem, len(res.Items))
+
+	for _, item := range res.Items {
+		wg.Add(1)
+		go func(item *youtube.PlaylistItem) {
+			defer wg.Done()
+			details, err := c.GetVideoPlayerDetails(item.Snippet.ResourceId.VideoId)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			if details.IsShort {
+				return
+			}
+			validVideos <- item
+		}(item)
+	}
+	wg.Wait()
+	close(errChan)
+	close(validVideos)
+
+	if len(errChan) > 0 {
+		return nil, <-errChan
+	}
+
+	var validVideosSlice []*youtube.PlaylistItem
+	for item := range validVideos {
+		validVideosSlice = append(validVideosSlice, item)
+	}
+
+	sort.Slice(validVideosSlice, func(i, j int) bool {
+		return validVideosSlice[i].Snippet.PublishedAt > validVideosSlice[j].Snippet.PublishedAt
+	})
+
+	var videos []yt.Video
+	for _, item := range validVideosSlice {
+		videos = append(videos, yt.Video{
+			ID:          item.Snippet.ResourceId.VideoId,
+			Title:       item.Snippet.Title,
+			Description: item.Snippet.Description,
+			Thumbnail:   item.Snippet.Thumbnails.Default.Url,
+			URL:         c.buildVideoEmbedURL(item.Snippet.ResourceId.VideoId),
+		})
+		if len(videos) >= limit {
+			break
+		}
+	}
+
+	return videos, nil
+}
