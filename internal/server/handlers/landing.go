7525491b1ca7c518b7108afa98c44fcd0e4c16dc
Cufee <vkouzin@outlook.com>
2023-10-23 17:04:56 -0500

    dockerfile timezone fix, minor updates

--- internal/server/handlers/landing.go
+++ internal/server/handlers/landing.go
@@ -1,22 +1,11 @@
 package root
 
 import (
-	"github.com/byvko-dev/youtube-app/internal/sessions"
 	"github.com/byvko-dev/youtube-app/internal/templates/pages"
 	"github.com/gofiber/fiber/v2"
 )
 
 func GerOrPosLanding(c *fiber.Ctx) error {
-	sessionId := c.Cookies("session_id")
-	if sessionId != "" {
-		session, _ := sessions.FromID(sessionId)
-		if session.Valid() {
-			return c.Redirect("/app")
-		}
-		session.Delete()
-		c.ClearCookie("session_id")
-	}
-
 	layout := "layouts/main"
 	if c.Method() == "POST" || c.Get("HX-Request") != "" {
 		layout = "layouts/blank"
