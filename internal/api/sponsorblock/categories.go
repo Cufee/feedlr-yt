49c3dc8362229c6cffe96a5638839f4ab7fd1519
Cufee <vkouzin@outlook.com>
2023-10-26 09:32:35 -0500

    clearer descriptions

--- internal/api/sponsorblock/categories.go
+++ internal/api/sponsorblock/categories.go
@@ -7,13 +7,13 @@ type Category struct {
 }
 
 var (
-	SelfPromo           = Category{"Self Promotion", "A segment that promotes the creator's own product or service.", "selfpromo"}
-	Interaction         = Category{"Interaction", "A segment that asks the viewer to interact with the video.", "interaction"}
-	Sponsor             = Category{"Sponsor", "A segment that promotes a product or service.", "sponsor"}
-	Preview             = Category{"Preview", "A segment that previews the video.", "preview"}
-	Intro               = Category{"Intro", "A segment that introduces the video.", "intro"}
-	Outro               = Category{"Outro", "A segment that concludes the video.", "outro"}
-	Music               = Category{"Music", "An offtopic music segment.", "music_offtopic"}
-	Filler              = Category{"Filler", "A filler segment.", "filler"}
-	AvailableCategories = []Category{SelfPromo, Interaction, Sponsor, Preview, Intro, Outro, Music, Filler}
+	SelfPromo           = Category{"Self Promotion", "Unpaid or self-promotion within the video, which may include content related to merchandise, donations, or information about collaborations.", "selfpromo"}
+	Interaction         = Category{"Interaction", "A brief reminder within the video to like, subscribe, or follow the content creator.", "interaction"}
+	Sponsor             = Category{"Sponsor", "Paid promotions, referrals, or direct advertisements featured in the video.", "sponsor"}
+	Preview             = Category{"Preview", "Clips that provide a glimpse of upcoming content or other videos in a series, with the information often repeated later in the video.", "preview"}
+	Intro               = Category{"Intro Animation", "An interval featuring no actual content, such as a pause, static frame, or repeating animation.", "intro"}
+	Outro               = Category{"Endcards", "The credits or appearance of YouTube endcards at the end of the video.", "outro"}
+	Music               = Category{"Music", "Segments featuring off-topic music in the video.", "music_offtopic"}
+	Filler              = Category{"Filler", "Tangential and humorous scenes added to the video that are not essential for understanding the main content.", "filler"}
+	AvailableCategories = []Category{Sponsor, SelfPromo, Interaction, Preview, Intro, Outro, Music, Filler}
 )
