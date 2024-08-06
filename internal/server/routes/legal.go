e4fd23916fdc6f264109918e1523ea566318f0ba
Vovko <vkouzin@outlook.com>
2024-08-06 15:39:59 -0400

    more minor bug fixes

---
+++ internal/server/routes/legal.go
@@ -0,0 +1,17 @@
+package root
+
+import (
+	"github.com/a-h/templ"
+	"github.com/cufee/feedlr-yt/internal/server/handler"
+	"github.com/cufee/feedlr-yt/internal/templates/components/shared"
+	"github.com/cufee/feedlr-yt/internal/templates/layouts"
+	"github.com/cufee/tpot/brewed"
+)
+
+var TermsOfService brewed.Page[*handler.Context] = func(ctx *handler.Context) (brewed.Layout[*handler.Context], templ.Component, error) {
+	return layouts.Main, shared.RemoteContentPage("https://byvko-dev.github.io/legal/terms-of-service-partial"), nil
+}
+
+var PrivacyPolicy brewed.Page[*handler.Context] = func(ctx *handler.Context) (brewed.Layout[*handler.Context], templ.Component, error) {
+	return layouts.Main, shared.RemoteContentPage("https://byvko-dev.github.io/legal/privacy-policy"), nil
+}
