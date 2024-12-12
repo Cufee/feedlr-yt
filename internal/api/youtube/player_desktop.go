e625e951bebcdd1171bf1cc9e3c2d8aefc335599
Vovko <vkouzin@outlook.com>
2024-12-11 22:07:13 -0500

    no error on private video

--- internal/api/youtube/player_desktop.go
+++ internal/api/youtube/player_desktop.go
@@ -64,20 +64,7 @@ func (c *client) getDesktopPlayerDetails(videoId string, tries ...int) (*VideoDe
 		return nil, err
 	}
 
-	transport := &http.Transport{}
-
-	var hasProxy bool
-	proxy, found := getPlayerProxy()
-	if found {
-		transport.Proxy = http.ProxyURL(proxy.url)
-		hasProxy = true
-	}
-
-	client := &http.Client{
-		Timeout:   time.Second * 10,
-		Transport: transport,
-	}
-
+	client := &http.Client{Timeout: time.Second * 10}
 	req, err := http.NewRequest("POST", playerURL.String(), body)
 	if err != nil {
 		return nil, err
@@ -86,9 +73,9 @@ func (c *client) getDesktopPlayerDetails(videoId string, tries ...int) (*VideoDe
 
 	res, err := client.Do(req)
 	if err != nil {
-		if hasProxy {
-			proxy.disableFor(time.Minute * 5)
-		}
+		// if hasProxy {
+		// 	proxy.disableFor(time.Minute * 5)
+		// }
 		if len(tries) < 1 || tries[0] < 2 {
 			return nil, err
 		}
@@ -100,9 +87,6 @@ func (c *client) getDesktopPlayerDetails(videoId string, tries ...int) (*VideoDe
 		return nil, err
 	}
 	if res.StatusCode != 200 {
-		if hasProxy {
-			proxy.disableFor(time.Minute * 5)
-		}
 		if len(tries) < 1 || tries[0] < 2 {
 			log.Debug().Str("body", string(responseBody)).Int("status", res.StatusCode).Msg("invalid response")
 			return nil, errors.New("GetVideoPlayerDetails.http.Post: invalid response")
@@ -115,13 +99,8 @@ func (c *client) getDesktopPlayerDetails(videoId string, tries ...int) (*VideoDe
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to parse response body")
 	}
-
 	if details.PlayabilityStatus.Status == "LOGIN_REQUIRED" {
-		if !hasProxy || len(tries) < 1 || tries[0] < 1 {
-			return nil, ErrLoginRequired
-		}
-		proxy.disableFor(time.Hour)
-		return c.GetVideoPlayerDetails(videoId, tries[0]-1)
+		log.Warn().Str("video", videoId).Str("message", details.PlayabilityStatus.Reason).Msg("login required to view content")
 	}
 
 	duration, _ := strconv.Atoi(details.PlayerVideoDetails.LengthSeconds)
@@ -150,7 +129,8 @@ func (c *client) getDesktopPlayerDetails(videoId string, tries ...int) (*VideoDe
 			fullDetails.Type = VideoTypeUpcomingStream
 		}
 		return &fullDetails, nil
-	} else if details.PlayabilityStatus.Status != "OK" || details.PlayerVideoDetails.IsPrivate {
+	}
+	if details.PlayabilityStatus.Status != "OK" || details.PlayerVideoDetails.IsPrivate {
 		fullDetails.Type = VideoTypePrivate
 		return &fullDetails, nil
 	}
